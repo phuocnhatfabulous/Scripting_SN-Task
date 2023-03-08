9c7e2b1d8038377cef2aa2a82061fb0f3fe01314
Phuoc Nhat <doanphuocnhat205@gmail.com>
2023-03-08 10:53:26 +0700

    06/03/23

---
+++ ThuyThao.js
@@ -0,0 +1,26 @@
+/**
+ * Task 1: 
+ * When user opens the existing incident and priority of the incident is P1 then there should be a message at the top that the current incident is P1
+ * Error message is "The current incident is a Priority 1 Incident"
+ */
+
+
+
+/**
+ * Task 2:
+ * If the short description contains SERVICENOW keyword in the beginning then Category should add a new choice "Snow and set it to the same"
+*/
+
+/**
+ * Task 3:
+ * If the caller of Incident is a VIP user then Urgency should automaticall set to High and there should be an alert to the user creating the incident
+ * Alert("The caller is a VIP user");
+ */
+
+
+/**
+ * Task 4:
+ * 1. If the Category changes to 'Inquiry/Help'. populate Subcategory is 'Email' and clear value if false.
+ * 2. If Category changes to 'Inquiry/Help', make field Description mandatory and revert if false.
+ * 3. If the user has 'itil' role, after they submit and incident, alert 'Thank you for your patience. We will keep you posted on the progress'
+ */
