e97890d853efbf4a3bc812d9c0327a7cc8930993
Phuoc Nhat <doanphuocnhat205@gmail.com>
2023-03-08 10:53:45 +0700

    07/03/23

---
+++ AnhKhoi.js
@@ -0,0 +1,53 @@
+/**
+ * Task 1:
+ * If the assignment group is changed to Service Desk, populate Assigned to is David Loo
+ */
+
+/**
+ * Task 2:
+ * When the user open an incident, alert message 'This is Inquiry Incident' if the Category is 'Inquiry/Help'
+ */
+
+/**
+ * Task 3:
+ * Cancel the submit if the work notes is empty
+ */
+
+
+// UI Policy
+
+/**
+ * Task 1:
+ * Make the Subcategory field Mandatory if the Category field in not None
+ */
+
+/**
+ * Task 2:
+ * Populate Assigned to is Beth Anglin if Assignment Group is Service Desk
+ * -> Compare the result with Exercise 1 in Client Script
+ */
+
+/**
+ * Task 3:
+ * Make the Subcategory field Readonly if the Service is not empty
+ * Test using these scenario
+ * S1: Change Category to None / Change Service to SAP Enterprise Services
+ * S2: Change Category to Inquiry/Help / Change Service to SAP Enterprise Services
+ */
+
+// UI Action
+
+/**
+ * Task 1:
+ * Create an Client UI Action to clear the Short Description
+ */
+
+/**
+ * Task 2:
+ * Create an Server UI Action to update the Business impact (business_impact) to "Not Impact"
+ */
+
+/**
+ * Task 3: 
+ * Merge 2 above UI Action
+ */
